e568279eb298c58400f40a5f7dde9266cf500c84
Piyush Saini <psaini2@cycle1.csug.rochester.edu>
2021-11-05 14:59:55 -0400

    hi

--- disassem.py
+++ disassem.py
@@ -10,3 +10,16 @@ obj_file.close()
 llvm_file = open('llvmoutput.txt', 'w')
 call(["llvm-dwarfdump", "--debug-line", sys.argv[1]], stdout= llvm_file)
 llvm_file.close()
+
+hexa_Pattern = re.compile("(\d|a|b|c|d|e|f){16}")
+
+
+#QuizList = []
+with open('llvmoutput.txt', 'r') as read_file:
+    for line in read_file:
+        parts = line.split()
+        print(len(parts))
+        if len(parts) > 0:
+            if hexa_Pattern.match(parts[0]):
+                print("uyyyyyy")
+    
