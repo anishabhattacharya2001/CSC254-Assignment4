afcbdb5fb56c4d52cb0367dd9b914b1c29de5070
Piyush Saini <psaini2@cycle1.csug.rochester.edu>
2021-11-05 16:03:49 -0400

    parsing done

--- disassem.py
+++ disassem.py
@@ -13,13 +13,23 @@ llvm_file.close()
 
 hexa_Pattern = re.compile("(\d|a|b|c|d|e|f){16}")
 
+hexa = re.compile("0x[0-9a-f]{16}")
 
-#QuizList = []
+
+# dictionaries 
+hexa_to_linenum = {}
+linenum_to_hex = {}
 with open('llvmoutput.txt', 'r') as read_file:
     for line in read_file:
         parts = line.split()
-        print(len(parts))
-        if len(parts) > 0:
-            if hexa_Pattern.match(parts[0]):
-                print("uyyyyyy")
+        if(len(parts) > 0):
+            if(hexa.match(parts[0])):
+                key = parts[0].replace('0x', '')
+                hexa_to_linenum[key] = parts[1]
+                linenum_to_hex[parts[1]] = key
+
+                
+                
+        
+        
     
