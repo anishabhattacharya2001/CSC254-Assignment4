3683ab3a5ed61185dab6699d9d93fe1d76e68e04
Anisha Bhattacharya <abhatta9@cycle1.csug.rochester.edu>
2021-11-05 13:31:32 -0400

    make llvm work

--- crossref.py
+++ crossref.py
@@ -1,13 +1,22 @@
 import os
 import re
+import sys
 from subprocess import call
 
 # need to run "objdump ‑d hello"
 # and run "llvm-dwarfdump --debug-line hello"
+#store their output in text file
+
+if(len(sys.argv) != 2):
+    print("found " + str(len(sys.argv)) + " arguments when we expect two.")
+    exit(1)
+
+executable_name = sys.argv[1]
+
 f = open('objdumpoutput.txt', 'w')
-call(["objdump", "-d", "hello"], stdout=f)
+call(["objdump", "-d", executable_name], stdout=f)
 f.close()
 
 f = open('llvmoutput.txt', 'w')
-call(["dwarfdump", "hello"], stdout=f)
+call(["llvm-dwarfdump", executable_name], stdout=f)
 f.close()
