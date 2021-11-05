9af493395061665c89c75c81a6098f8ef240874e
Piyush Saini <psaini2@cycle1.csug.rochester.edu>
2021-11-05 14:06:02 -0400

    objDum works pp

--- disassem.py
+++ disassem.py
@@ -7,14 +7,11 @@ import re
 # and run "llvm-dwarfdump --debug-line hello"
 #store their output in text file
 
-# if(len(sys.argv) != 2):
-#     print("found " + str(len(sys.argv)) + " arguments when we expect two.")
-#     exit(1)
 
 f = open('objdumpoutput.txt', 'w')
 call(["objdump", "-d", sys.argv[1]], stdout=f)
 f.close()
 
 f = open('llvmoutput.txt', 'w')
-call(["llvm-dwarfdump", sys.argv[1]], stdout=f)
+call(["llvm-dwarfdump", "--debug-line", sys.argv[1]], stdout=f)
 f.close()
