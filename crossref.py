f0c569e50053dd9081053879309180f0376a53cf
Anisha Bhattacharya <abhatta9@cycle1.csug.rochester.edu>
2021-11-05 13:14:57 -0400

    do work

--- crossref.py
+++ crossref.py
@@ -2,6 +2,8 @@ import os
 import re
 from subprocess import call
 
+# need to run "objdump ‑d hello"
+# and run "llvm-dwarfdump --debug-line hello"
 f = open('objdumpoutput.txt', 'w')
 call(["objdump", "-d", "hello"], stdout=f)
 f.close()
