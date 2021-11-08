49f241285eba83dcbf9a13df702dfae926c15272
Anisha Bhattacharya <abhatta9@cycle1.csug.rochester.edu>
2021-11-08 10:08:08 -0500

    done dana done

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,2 +1,18 @@
+Anisha Bhattacharya (abhatta9)
+Piyush Saini (psaini2)
 
-run "python disassem.py hello"
+CSC254 - Assignment4
+
+To run this file first run (you can try -g0)
+gcc -g3 -o myprogram myfile1.c myfile2.c ... 
+And then
+Python disassem.py myprogram
+
+For each file that you have created there will be a separate html file created corresponding to its name. The webpage contains side-by-side assembly language and corresponding source code for a given program in a table. 
+
+The output of commands llvm-dwarfdump and objdump ‑d  is stored in a text file and using the table of line to assembly code we look into object dump to find the range of commands associated with a particular instruction. 
+
+There is a separate array created for each file's llvm table content to separate the information.
+
+For each address in the objdump we have added an id to it and anytime there is a callq or jmp (or other control flow instructions) an href tag is added to which makes it jump to the address required.
+
--- disassem.py
+++ disassem.py
@@ -7,18 +7,13 @@ import datetime
 
 #find time taken to run
 running = str(datetime.datetime.now())
-
-
 obj_file = open('objdumpoutput.txt', 'w')
 call(["objdump", "-d", sys.argv[1]], stdout = obj_file )
 obj_file.close()
-
 llvm_file = open('llvmoutput.txt', 'w')
 call(["llvm-dwarfdump", "--debug-line", sys.argv[1]], stdout= llvm_file)
 llvm_file.close()
-
 hexa_Pattern = re.compile("(\d|a|b|c|d|e|f){16}")
-
 hexa = re.compile("0x[0-9a-f]{16}")
 small_hexa = re.compile("[0-9a-f]")
 
@@ -52,13 +47,17 @@ with open('llvmoutput.txt', 'r+') as f:
             fileandtable[fn] = tablecontent          
 
 arrTables = []
+for tab in arrTables:
+    print("FILE START")
+    print(tab)
+
 #put the dwarfdump output of each file into a separate text file
 for i in range( len(filename) ):
     file = open(filename[i]+"output.txt", "w")
     file.writelines(fileandtable.get(filename[i]))
     arrTables.append(fileandtable.get(filename[i]))
 
-
+hex_and_func_regex = re.compile("[0-9a-f]{16}\s<.+>:")
 
 def everything(filen, n):
     # dictionaries 
@@ -98,10 +97,8 @@ def everything(filen, n):
             if(len(parts) > 0):
                 if(small_hexa.match(parts[0])):
                     key = parts[0].replace(':', '')
-                    Assembly[key] = parts
+                    Assembly[key] = line
     Assembly_Addresses = sorted(Assembly)
-
-
 
     # creating a dict with line and the range
     assembly_Array = {}
@@ -121,9 +118,6 @@ def everything(filen, n):
                 ek_list.append(Assembly_Addresses[i])
         Line_to_Addresses[key] = ek_list
 
-
-
-
     line_to_cCode = {}
     i = 1
     with open(filen, 'r') as read_file:
@@ -132,10 +126,7 @@ def everything(filen, n):
                 line_to_cCode[i] = line
             i = i + 1 
 
-
-
     # # FINAL C CODE to Assembly code 
-    # code : list of list of list
     Final_C_to_Assembly = {}
     i = 1
     with open(filen, 'r') as read_file:
@@ -156,51 +147,67 @@ def everything(filen, n):
     # Function to convert   
     def listToString(lis):    
         s = ""
-        if(lis is "No Assembly code reference found for this source"):
+        if(lis == "No Assembly code reference found for this source"):
             return "No Assembly code reference found for this source"
         for ele in lis:  
             s += ele
             s += " "        
         return s
 
+    small_hexa_col = re.compile("[0-9a-f]:")
+    def func_Div(ass):
+        A = ass.split()
+        retString = ''
+        if(small_hexa.match(A[0])):
+            stringg = '<div id=' + A[0] + '>' + A[0] + '</div>'
+            A[0] = stringg
+            retString = " ".join(A)
+            return retString
+        else:
+            return ass
+
+    control_flow = ["jmp", "je", "jne", "jz", "jg", "jge", "jl", "jle", "callq"]
+    def func_href(abss):
+        retString = ''
+        for cf in control_flow:
+            if cf in abss:
+                A = abss.split()
+                for i in range(len(A)):
+                    if(A[i] == cf):
+                        something = '<a href= \" #' + A[i + 1] + ':\">' + A[i + 1] + '</a>'
+                        A[i + 1] = something
+                        retString = " ".join(A)
+                        return retString     
+        return abss 
+
     path = os.path.dirname(os.path.realpath(__file__))
-
-    # <hr><br><br>
-    #         <strong>Run Time: </strong>"""+timeRan+"""<br>
-    #         <strong>Location: </strong>"""+path+ """<br>
-    #     <br><br>
-
-    #<style>table, th, td {border:1px solid black;}</style>
-    #indexFile.write("<html><head><title>CSC254</title></head><body><h1>Disassemble<h1><table style="width:100%"><tr><th>Source</th><th>Assessmbly</th></tr></table></body></html>")
-
-
     fileout = open(filen+"index.html", "w")
-
     htmlcode = "<html> \n <style>\n"
     htmlcode += "table, th, td {border:1px solid black;}\n </style>"
+    htmlcode += "<hr>\n"
+    htmlcode += "  Time Ran: "+running+ "<br>"
+    htmlcode += "  Location: "+path+"<br>"
     htmlcode += "<body>\n <h1>Disassemble<h1>"
 
     # Create table
-    table = "<table style=\"width:100%\">"
+    table = "<table style=\"width:100%\">  \n"
     table += "  <tr>\n"
     table += "  <th>source</th>\n"
     table += "  <th>assembly</th>\n"
     table += "  </tr>\n"
-    
-
     for key, value in Final_C_to_Assembly.items():
         table += " <tr> \n"
         table += "  <td>"+key+" </td> \n"
         ass = " <td>"
-        for assemblyLineList in value:
-            ass += listToString(assemblyLineList)
+        for assemblyLine in value:
+            abss = func_Div(assemblyLine)
+            abb = func_href(abss)
+            ass += abb
             ass += '\n<br>'
         ass += "</td>"
         table += ass
         table += "  </tr>\n"
-
     table += "</table>"
-
     htmlcode += table
     htmlcode += "</body> \n </html>"
     fileout.writelines(htmlcode)
